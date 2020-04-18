e2c65537a79c84172214ee4a0b5ad29a728c08e4
Otávio Luis Martins <otavioluism97@hotmail.com>
2020-04-18 16:38:15 -0300

    Project that add, remove repository at front-end

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from 'services/api';
 
 import "./styles.css";
 
+import deleteImage from "./assets/delete.png";
+import aceptImage from "./assets/acept.png";
+
+
 function App() {
+  
+  const [repositories, setRepositories] = useState([]);
+
+  //Open componente
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  //Button add repository
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post("/repositories", {
+      url: "https://github.com",
+      title: `New Project ${Date.now()}`,
+      techs: ["Node.js", "ReactJS", "React Native"]
+    });
+    
+    const repository = response.data; 
+
+    //imutabilidade
+    setRepositories([...repositories, repository]);
+
   }
 
+  //Button remove repository
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    try{
+
+      await api.delete(`repositories/${id}`);
+      
+      setRepositories(repositories.filter(repository => repository.id !== id));
+
+    }catch{ 
+
+      alert("error the delet repository")
+
+    }
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+    <div>
+
+      <ul data-testid="repository-list">        
+          {repositories.map(repository =>        
+             <li key={repository.id}>
+                  {repository.title}  
+                  <button onClick={() => handleRemoveRepository(repository.id)}> 
+                      Remover 
+                      <img src={deleteImage} alt="Icone de Remove"/>
+                  </button>
+              </li>
+           )}
+
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>
+        Adicionar
+        <img src={aceptImage} alt="Icone de Acept"/>
+      </button>
+      
     </div>
   );
 }
